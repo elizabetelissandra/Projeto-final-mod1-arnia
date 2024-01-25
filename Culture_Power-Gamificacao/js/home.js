c3febd4863412cee7f9f7b359bbe696284618527
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-01-24 21:14:10 -0300

    update home.css, home.html and home.js

--- Culture_Power-Gamificacao/js/home.js
+++ Culture_Power-Gamificacao/js/home.js
@@ -2,6 +2,64 @@ const resgatarProduto = (id) => {
         window.location.href = `../html/produto.html?id=${id}`
     }
 
+const filtrarProdutos = (produtos) =>{
+    const listaProdutos = document.querySelector('.listaProdutos')
+
+    produtos.forEach(produto =>{
+        let itemProduto = document.createElement('li');
+        itemProduto.innerHTML += 
+        `
+        <div class='itemProduto'>
+        <a href="../html/produto.html?id=${produto.id}">
+            <img src='${produto.imagem}'>
+            <h2>${produto.nome}</h2>
+        </a>
+        `
+        listaProdutos.appendChild(itemProduto)
+    })
+}    
+
+const filtrar = () =>{
+    //pegar elementos do html
+    let ul = document.querySelector('.listaProdutos')
+    //pegar as li do objeto
+    const li = ul.getElementsByTagName('li')
+    
+    // filtrar os valores
+    const busca = document.getElementById("buscar").value.toUpperCase();
+    let count = 0;
+    let span
+    //percorrer as li's
+    for(let i = 0; i< li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        let txtValue = a.textContent || a.innerText;
+        //Verificando se o usuario digitou bate
+        if(txtValue.toUpperCase().indexOf(busca) > -1
+        && busca != ''){
+            li[i].style.display = "";
+            //se o valor bater adicionar mais 1 no count
+            count++
+            //depois pegar a tag span
+            span = li[i].querySelector('.listaDeProdutos')
+            //se existir
+            if(span){
+                span.innerHTML = txtValue.replace(new RegExp(busca, 'gi'),(match)=>{
+                    return '<strong>' + match + '</strong>'
+                })
+            }
+            }else{
+                //nao mostrar o item da lista
+                li[i].style.display = "none";
+                }
+    }
+    if(count === 0){
+        ul.style.display = 'none';
+    }else{
+        ul.style.display = 'flex';
+    }
+    
+}
+
 const mostrarDados = (produtos) => {
     const divProdutos = document.querySelector('.produtosResgatar')
 
@@ -28,5 +86,6 @@ const carregarDados = async() => {
     console.log(produtos)
 
     mostrarDados(produtos)
+    filtrarProdutos(produtos)
 }
 carregarDados()
